a2c65bf3b211e8e0728f99136c602a101f075f42
hugo landre <hugolandre7@gmail.com>
2022-03-04 14:11:48 +0100

    truc nul

--- sonnette/Program.cs
+++ sonnette/Program.cs
@@ -8,20 +8,17 @@ public class Program {
         GpioController controller = new GpioController(PinNumberingScheme.Board);
 
         controller.OpenPin(10, PinMode.Output);
-        controller.OpenPin(9, PinMode.Input);
+        controller.OpenPin(12, PinMode.Input);
+
+        PinValue prevPinValue = PinValue.Low;
+        bool isOn = false;
 
         while (true)
         {
-            var button = controller.Read(9);
-            if(button == PinValue.High)
-                controller.Write(10, PinValue.High);
-            else
-                controller.Write(10, PinValue.Low);
-
-
-            //Thread.Sleep(1000);
-            //Thread.Sleep(1000);
-
+            PinValue button = controller.Read(12);
+            if (prevPinValue == PinValue.Low && button == PinValue.High)
+                isOn = !isOn;
+            prevPinValue = button;
         }
     }
 }
