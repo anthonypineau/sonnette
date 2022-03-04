6cc93fb876e198d8528fdf4ffc8800b21cf77fc4
hugo landre <hugolandre7@gmail.com>
2022-03-04 11:25:28 +0100

    test

--- sonnette/Program.cs
+++ sonnette/Program.cs
@@ -1,7 +1,21 @@
-﻿namespace sonnette.rasppi;
+﻿using System.Device.Gpio;
+
+namespace sonnette.rasppi;
 
 public class Program {
     public static void Main(string[] args) {
         Console.WriteLine("Hello, World!");
+        GpioController controller = new GpioController(PinNumberingScheme.Board);
+
+        controller.OpenPin(10, PinMode.Output);
+
+        while (true)
+        {
+            controller.Write(10, PinValue.Low);
+            Thread.Sleep(1000);
+            controller.Write(10, PinValue.High);
+            Thread.Sleep(1000);
+
+        }
     }
 }
