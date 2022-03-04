147e3f81f3f08827e7a5e45a5013e623782a0920
Anthony PINEAU <anthonypineau85@gmail.com>
2022-03-04 15:51:03 +0100

    rtc chat

---
+++ rtc/Hubs/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace sonnette.rtc.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+        }
+    }
+}
