b47daeb41afda05017d266a2e8421997d9778446
hugo landre <hugolandre7@gmail.com>
2022-03-04 13:30:04 +0100

    bouton

--- sonnette/Program.cs
+++ sonnette/Program.cs
@@ -8,13 +8,19 @@ public class Program {
         GpioController controller = new GpioController(PinNumberingScheme.Board);
 
         controller.OpenPin(10, PinMode.Output);
+        controller.OpenPin(9, PinMode.Input);
 
         while (true)
         {
-            controller.Write(10, PinValue.Low);
-            Thread.Sleep(1000);
-            controller.Write(10, PinValue.High);
-            Thread.Sleep(1000);
+            var button = controller.Read(9);
+            if(button == PinValue.High)
+                controller.Write(10, PinValue.High);
+            else
+                controller.Write(10, PinValue.Low);
+
+
+            //Thread.Sleep(1000);
+            //Thread.Sleep(1000);
 
         }
     }
