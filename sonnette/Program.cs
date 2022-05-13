dc66fce3f1a300267bd1381c21353bd9d1da48da
Anthony PINEAU <anthonypineau85@gmail.com>
2022-05-13 09:21:46 +0200

    update

--- sonnette/Program.cs
+++ sonnette/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.Extensions.Configuration;
 using sonnette.rasppi.Models;
 using System.Device.Gpio;
+using System.Device.Pwm;
 using System.Net;
 using System.Net.Http.Headers;
 using System.Net.Http.Json;
@@ -38,11 +39,11 @@ public class Program {
 
         //SERVO
         // LOW HELICE MEME SENS BOITIER
-        controller.OpenPin(pinServoMotor, PinMode.Output);
+        // controller.OpenPin(pinServoMotor, PinMode.Output);
         //controller.OpenPin(pinServoMotor, PinMode.Input);
         //Servo servo = new Servo(Device.CreatePwmPort(Device.Pins.D08), NamedServoConfigs.SG90);
         //Console.WriteLine(controller.Read(pinServoMotor));
-        controller.Write(pinServoMotor, PinValue.High);
+        //controller.Write(pinServoMotor, PinValue.High);
 
         PinValue button;
         PinValue prevButton = false;
@@ -50,24 +51,28 @@ public class Program {
         while (true) {
             button = controller.Read(pinButton);
             if (button == PinValue.Low && prevButton != button) {
-                RunAsync().GetAwaiter().GetResult();
+                RunAsync(controller, pinTransistor).GetAwaiter().GetResult();
+                /*
                 isOn = !isOn;
                 if (isOn) {
                     controller.Write(pinTransistor, PinValue.High);
                 } else {
                     controller.Write(pinTransistor, PinValue.Low);
                 }
+                */
             }
             prevButton= controller.Read(pinButton);
             Thread.Sleep(100);
         }
     }
 
-    static async Task RunAsync() {
+    static async Task RunAsync(GpioController controller, int pinValue) {
         HttpClient client = new HttpClient();
         client.BaseAddress = new Uri(apiUri);
         client.DefaultRequestHeaders.Accept.Clear();
         client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+        bool isGood = false;
 
         try {
             Sonnette sonnette = new Sonnette {
@@ -77,11 +82,57 @@ public class Program {
             };
 
             var statusCode = await CreateProductAsync(sonnette, client);
+            switch (statusCode)
+            {
+                case HttpStatusCode.OK:
+                case HttpStatusCode.Created:
+                case HttpStatusCode.Accepted:
+                case HttpStatusCode.NonAuthoritativeInformation:
+                case HttpStatusCode.NoContent:
+                case HttpStatusCode.ResetContent:
+                    isGood = true;
+                    break;
+
+            }
             Console.WriteLine($"Status code returned {statusCode} ({(int)statusCode})");
         }
         catch (Exception e)
         {
             Console.WriteLine(e.Message);
+        }
+
+        if (isGood)
+        {
+            controller.Write(pinTransistor, PinValue.High);
+            Thread.Sleep(1000);
+            controller.Write(pinTransistor, PinValue.Low);
+        }
+        else
+        {
+            controller.Write(pinTransistor, PinValue.High);
+            Thread.Sleep(100);
+            controller.Write(pinTransistor, PinValue.Low);
+            Thread.Sleep(100);
+            controller.Write(pinTransistor, PinValue.High);
+            Thread.Sleep(100);
+            controller.Write(pinTransistor, PinValue.Low);
+            Thread.Sleep(100);
+            controller.Write(pinTransistor, PinValue.High);
+            Thread.Sleep(100);
+            controller.Write(pinTransistor, PinValue.Low);
+            Thread.Sleep(100);
+            controller.Write(pinTransistor, PinValue.High);
+            Thread.Sleep(100);
+            controller.Write(pinTransistor, PinValue.Low);
+            Thread.Sleep(100);
+            controller.Write(pinTransistor, PinValue.High);
+            Thread.Sleep(100);
+            controller.Write(pinTransistor, PinValue.Low);
+            Thread.Sleep(100);
+            controller.Write(pinTransistor, PinValue.High);
+            Thread.Sleep(100);
+            controller.Write(pinTransistor, PinValue.Low);
+            Thread.Sleep(100);
         }
     }
 
